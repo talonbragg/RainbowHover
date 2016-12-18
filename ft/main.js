044c3e608ae1bee2db345daaa4c3dd9e5c19a3dc
Talon Bragg <pvpgamecrash@gmail.com>
2016-12-18 10:31:40 -0800

    Update main.js

--- ft/main.js
+++ ft/main.js
@@ -3,7 +3,8 @@ function GetValue()
     var myarray= new Array("Maybe","Yes","No","It depends","Definetely Not","It is possible", "duh", "Don't Ask me!");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
-}
-if (event.keyCode === 13) {
+   if (event.keyCode == 13) {
     document.getElementById().innerHTML = "Don't Use Enter!";
     }
+}
+
