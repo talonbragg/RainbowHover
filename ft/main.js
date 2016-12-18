85f4f636f1906390a1bcf61977f7e7e05d3e6c9a
Talon Bragg <pvpgamecrash@gmail.com>
2016-12-17 21:09:51 -0800

    Update main.js

--- ft/main.js
+++ ft/main.js
@@ -1,6 +1,6 @@
 function GetValue()
 {
-    var myarray= new Array("Maybe","Yes","No","It depends","Definetely Not","Get A Life!");
+    var myarray= new Array("Maybe","Yes","No","It depends","Definetely Not","It is possible", "duh", "Don't Ask me!");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
 }
